9582701adf42873e77b01e482d9c74923deab25b
Seongbin Kim <seongbin9786@gmail.com>
2018-11-12 12:12:05 +0900

    프로젝트 생성

---
+++ spomatch-backend/src/main/java/com/spomatch/SpomatchApplication.java
@@ -0,0 +1,12 @@
+package com.spomatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpomatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpomatchApplication.class, args);
+	}
+}
