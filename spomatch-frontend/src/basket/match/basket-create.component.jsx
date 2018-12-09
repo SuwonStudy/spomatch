dbbc8e677f1f7d28b6d1c416ec19fbd3355dffd3
circleGiven <circlegiven@gmail.com>
2018-12-09 14:53:57 +0900

    #18 add field in create match component

--- spomatch-frontend/src/basket/match/basket-create.component.jsx
+++ spomatch-frontend/src/basket/match/basket-create.component.jsx
@@ -4,6 +4,9 @@ import classNames from 'classnames';
 import { withStyles } from '@material-ui/core/styles';
 import MenuItem from '@material-ui/core/MenuItem';
 import TextField from '@material-ui/core/TextField';
+import IconButton from '@material-ui/core/IconButton';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import Search from '@material-ui/icons/Search';
 
 const styles = theme => ({
     container: {
@@ -15,32 +18,18 @@ const styles = theme => ({
         marginRight: theme.spacing.unit,
         width: 200,
     },
-    dense: {
-        marginTop: 19,
-    },
     menu: {
         width: 200,
     },
 });
 
-const currencies = [
-    {
-        value: 'USD',
-        label: '$',
-    },
-    {
-        value: 'EUR',
-        label: '€',
-    },
-    {
-        value: 'BTC',
-        label: '฿',
-    },
-    {
-        value: 'JPY',
-        label: '¥',
-    },
-];
+const attendee = [
+    { label: '3 vs 3', value: 3},
+    { label: '4 vs 4', value: 4},
+    { label: '5 vs 5', value: 5},
+    { label: '6 vs 6', value: 6},
+    { label: '7 vs 7', value: 7}
+    ];
 
 class BasketCreateComponent extends Component {
     state = {
@@ -48,6 +37,7 @@ class BasketCreateComponent extends Component {
         age: '',
         multiline: 'Controlled',
         currency: 'EUR',
+        attendee: 3,
     };
 
     handleChange = name => event => {
@@ -56,195 +46,75 @@ class BasketCreateComponent extends Component {
         });
     };
 
+    handleClickPlace = () => {
+
+    };
+
     render() {
         const { classes } = this.props;
 
         return (
-            <form className={classes.container} noValidate autoComplete="off">
-                <TextField
-                    id="standard-name"
-                    label="Name"
-                    className={classes.textField}
-                    value={this.state.name}
-                    onChange={this.handleChange('name')}
-                    margin="normal"
-                />
-                <TextField
-                    id="standard-uncontrolled"
-                    label="Uncontrolled"
-                    defaultValue="foo"
-                    className={classes.textField}
-                    margin="normal"
-                />
+            <form className={classes.container} autoComplete="off">
+                {/* 매칭 제목 */}
                 <TextField
                     required
-                    id="standard-required"
-                    label="Required"
-                    defaultValue="Hello World"
-                    className={classes.textField}
-                    margin="normal"
-                />
-                <TextField
-                    error
-                    id="standard-error"
-                    label="Error"
-                    defaultValue="Hello World"
-                    className={classes.textField}
-                    margin="normal"
-                />
-                <TextField
-                    disabled
-                    id="standard-disabled"
-                    label="Disabled"
-                    defaultValue="Hello World"
-                    className={classes.textField}
-                    margin="normal"
-                />
-                <TextField
-                    id="standard-password-input"
-                    label="Password"
-                    className={classes.textField}
-                    type="password"
-                    autoComplete="current-password"
-                    margin="normal"
-                />
-                <TextField
-                    id="standard-read-only-input"
-                    label="Read Only"
-                    defaultValue="Hello World"
-                    className={classes.textField}
-                    margin="normal"
-                    InputProps={{
-                        readOnly: true,
-                    }}
-                />
-                <TextField
-                    id="standard-dense"
-                    label="Dense"
-                    className={classNames(classes.textField, classes.dense)}
-                    margin="dense"
-                />
-                <TextField
-                    id="standard-multiline-flexible"
-                    label="Multiline"
-                    multiline
-                    rowsMax="4"
-                    value={this.state.multiline}
-                    onChange={this.handleChange('multiline')}
-                    className={classes.textField}
-                    margin="normal"
-                />
-                <TextField
-                    id="standard-multiline-static"
-                    label="Multiline"
-                    multiline
-                    rows="4"
-                    defaultValue="Default Value"
-                    className={classes.textField}
-                    margin="normal"
-                />
-                <TextField
-                    id="standard-helperText"
-                    label="Helper text"
-                    defaultValue="Default Value"
-                    className={classes.textField}
-                    helperText="Some important text"
-                    margin="normal"
-                />
-                <TextField
-                    id="standard-with-placeholder"
-                    label="With placeholder"
-                    placeholder="Placeholder"
-                    className={classes.textField}
-                    margin="normal"
-                />
-                <TextField
-                    id="standard-textarea"
-                    label="With placeholder multiline"
-                    placeholder="Placeholder"
-                    multiline
-                    className={classes.textField}
-                    margin="normal"
-                />
-                <TextField
-                    id="standard-number"
-                    label="Number"
-                    value={this.state.age}
-                    onChange={this.handleChange('age')}
-                    type="number"
-                    className={classes.textField}
-                    InputLabelProps={{
-                        shrink: true,
-                    }}
-                    margin="normal"
-                />
-                <TextField
-                    id="standard-search"
-                    label="Search field"
-                    type="search"
-                    className={classes.textField}
-                    margin="normal"
-                />
-                <TextField
-                    id="standard-select-currency"
-                    select
-                    label="Select"
-                    className={classes.textField}
-                    value={this.state.currency}
-                    onChange={this.handleChange('currency')}
-                    SelectProps={{
-                        MenuProps: {
-                            className: classes.menu,
-                        },
-                    }}
-                    helperText="Please select your currency"
-                    margin="normal"
-                >
-                    {currencies.map(option => (
-                        <MenuItem key={option.value} value={option.value}>
-                            {option.label}
-                        </MenuItem>
-                    ))}
-                </TextField>
-                <TextField
-                    id="standard-select-currency-native"
-                    select
-                    label="Native select"
-                    className={classes.textField}
-                    value={this.state.currency}
-                    onChange={this.handleChange('currency')}
-                    SelectProps={{
-                        native: true,
-                        MenuProps: {
-                            className: classes.menu,
-                        },
-                    }}
-                    helperText="Please select your currency"
-                    margin="normal"
-                >
-                    {currencies.map(option => (
-                        <option key={option.value} value={option.value}>
-                            {option.label}
-                        </option>
-                    ))}
-                </TextField>
-                <TextField
-                    id="standard-full-width"
-                    label="Label"
+                    id="match-title"
+                    label="매칭 제목"
                     style={{ margin: 8 }}
-                    placeholder="Placeholder"
-                    helperText="Full width!"
                     fullWidth
                     margin="normal"
                     InputLabelProps={{
                         shrink: true,
+                    }}/>
+                {/* 장소 */}
+                <TextField
+                    disabled
+                    id="match-place"
+                    label="장소"
+                    className={classes.textField}
+                    margin="normal"
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                    InputProps={{
+                        endAdornment: (
+                            <InputAdornment variant="filled" position="end">
+                                <IconButton
+                                    aria-label="매칭 장소 검색"
+                                    onClick={this.handleClickPlace}>
+                                    <Search/>
+                                </IconButton>
+                            </InputAdornment>
+                        ),
                     }}
                 />
+                {/* 경기 참여 인원 */}
                 <TextField
-                    id="standard-bare"
+                    id="match-attendee"
+                    select
+                    label="참여 인원"
                     className={classes.textField}
-                    defaultValue="Bare"
+                    value={this.state.attendee}
+                    onChange={this.handleChange('attendee')}
+                    SelectProps={{
+                        MenuProps: {
+                            className: classes.menu,
+                        },
+                    }}
+                    helperText="매칭 참여 인원수를 선택하세요"
+                    margin="normal">
+                    {attendee.map(item => <MenuItem key={item.label} value={item.value}>{item.label}</MenuItem>)}
+                </TextField>
+                {/* 경기 날짜 설정 */}
+                <TextField
+                    id="match-time"
+                    label="경기 날짜"
+                    type="datetime-local"
                     margin="normal"
+                    className={classes.textField}
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
                 />
             </form>
         );
