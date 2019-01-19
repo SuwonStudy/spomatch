c6e52df82d45395a5cffd9d279434e3901686fe8
circleGiven <circlegiven@gmail.com>
2019-01-19 14:52:27 +0900

    #23 create routes.js

---
+++ spomatch-frontend/src/Routes.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Switch, Route, HashRouter } from 'react-router-dom';
+
+export default () => (
+    <HashRouter>
+        <Switch>
+        </Switch>
+    </HashRouter>
+);
